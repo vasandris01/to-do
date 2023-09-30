f8064ed0e9d55e8e1a602e85c1bd387099652502
vasandris01 <undris01@gmail.com>
2023-09-30 13:31:11 +0200

    v1.0

--- src/main/java/com/example/todo/model/User.java
+++ src/main/java/com/example/todo/model/User.java
@@ -1,21 +1,14 @@
 package com.example.todo.model;
 
-
-
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.util.List;
-
-
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,7 +23,7 @@ public class User {
     private String email;
     private String password;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<ToDo> toDos;
 }
 
